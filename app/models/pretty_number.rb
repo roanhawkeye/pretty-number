aa6841a8f9746239c99682150fddcf8c478aa4a1
Daniel Martinez <roanhawkeye@gmail.com>
2015-10-07 23:55:47 -0500

    Create migration for model, and add some validations for presence of attributes and uniqueness

--- app/models/pretty_number.rb
+++ app/models/pretty_number.rb
@@ -1,2 +1,5 @@
 class PrettyNumber < ActiveRecord::Base
+	validates :name, :abbreviation_name, :number_min_length, :number_max_length, presence: true
+	validates :name, uniqueness: { case_sensitive: false }
+	validates :number_min_length, :number_max_length, uniqueness: true
 end
--- db/migrate/20151008004602_create_pretty_numbers.rb
+++ db/migrate/20151008004602_create_pretty_numbers.rb
@@ -1,7 +1,10 @@
 class CreatePrettyNumbers < ActiveRecord::Migration
   def change
     create_table :pretty_numbers do |t|
-
+      t.string :name
+      t.string :abbreviation_name
+      t.integer :number_min_length
+      t.integer :number_max_length	
       t.timestamps null: false
     end
   end
