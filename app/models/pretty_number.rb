833cc8c697c37547e04b348ddadfebe266a18259
Daniel Martinez <roanhawkeye@gmail.com>
2015-10-08 15:36:07 -0500

    Refactor prettify logic to be cleaner and DRY

--- app/models/pretty_number.rb
+++ app/models/pretty_number.rb
@@ -4,7 +4,6 @@ class PrettyNumber < ActiveRecord::Base
 	validates :number_min_length, :number_max_length, uniqueness: true
 
 	def self.prettify(raw_number)
-		scales = self.get_available_scales()
 		number = self.clean_number(raw_number)
 
 		# Cache length in order to do not calculate it with every iteration
@@ -14,14 +13,8 @@ class PrettyNumber < ActiveRecord::Base
 		if number_length <= 6
 			return number
 		else
-			# searchs for a fit scale
-			scales.each do |scale|
-				if number_length >= scale.number_min_length && number_length <= scale.number_max_length
-					return self.build_pretty_version(number, scale)
-				end
-			end
+			return self.find_pretty_version(number_length, number)
 		end
-		"Sorry, there is no available scale to prettify your number, please try again with different one"
 	end
 
 	private
@@ -30,23 +23,45 @@ class PrettyNumber < ActiveRecord::Base
 			self.all
 		end
 
-		# It cleans raw number for decimals part, to be prepare to prettify
+		# It cleans raw number for decimals part to be prepare for prettifying, returns a string
 		def self.clean_number(raw_number)
 			number = raw_number.to_i.to_s
 		end
 
 		#Reduces number to 1 decimal if need it, and append right notation
 		def self.build_pretty_version(number, scale)
+			# Cache variables to increase performance
 			lenght_difference = number.length - scale.number_min_length
-			puts "lenght_difference: #{lenght_difference}"
+			scale_firt_decimal_index = lenght_difference + 1
+			prettify_version = ''
+			elements = []
 
-			if lenght_difference >= 1
-				reduced_number = number[0,lenght_difference + 1] + scale.abbreviation_name
-			else
-				reduced_number = number[0] + scale.abbreviation_name
+			# truncate scale extra position before adding notation
+			reduced_number = number[0,scale_firt_decimal_index]
+			elements.append(reduced_number)
+
+			#Verifies that an aditional decimal point and number is need it and add it
+			if number[scale_firt_decimal_index].to_s != '0'
+				elements.append(".#{number[scale_firt_decimal_index]}")
 			end
-			puts reduced_number
-			reduced_number
+
+			#Add scale notation to end of prettify version
+			elements.append(scale.abbreviation_name)
+
+			elements.each do |element|
+				prettify_version += element
+			end
+			prettify_version
 		end
 
+		def self.find_pretty_version(number_length, number)
+			scales = self.get_available_scales()
+
+			scales.each do |scale|
+				if number_length >= scale.number_min_length && number_length <= scale.number_max_length
+					return self.build_pretty_version(number, scale)
+				end
+			end
+			"Sorry, there is no available scale to prettify your number, please try again with different one"
+		end
 	end
