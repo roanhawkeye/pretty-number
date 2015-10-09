283e1d112b9bb8035d563a869404e367d24db069
Daniel Martinez <roanhawkeye@gmail.com>
2015-10-09 10:25:45 -0500

    Change message in welcome page and add controller actions for CRUD

--- app/controllers/pretty_numbers_controller.rb
+++ app/controllers/pretty_numbers_controller.rb
@@ -10,4 +10,54 @@ class PrettyNumbersController < ApplicationController
 	      format.js   { render :action => "prettify" }
 	    end
 	end
+
+	def show
+		@pretty_number = PrettyNumber.find(params[:id])
+	end
+
+	def new
+		@pretty_number = PrettyNumber.new
+	end
+
+	def edit
+		@pretty_number = PrettyNumber.find(params[:id])
+	end
+
+	def create
+		@pretty_number = PrettyNumber.new(pretty_number_params)
+
+		if @pretty_number.save
+			redirecto_to @pretty_number
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		@pretty_number = PrettyNumber.find(params[:id])
+
+		if @pretty_number.update
+			redirecto_to @pretty_number
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@pretty_number = PrettyNumber.find(params[:id])
+		@pretty_number.destroy
+
+		redirecto_to pretty_numbers_path
+	end
+
+	private
+
+		def pretty_number_params
+			params.require(:pretty_number).permit(
+				:name,
+				:abbreviation_name,
+				:number_min_length,
+				:number_max_length
+			)
+		end
 end
