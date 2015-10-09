18f3173e664de075027bbecfed807efafc88de22
Daniel Martinez <roanhawkeye@gmail.com>
2015-10-09 01:03:22 -0500

    Add funtional test to controllers

--- test/controllers/pretty_number_controller_test.rb
+++ test/controllers/pretty_number_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class PrettyNumberControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@controller = PrettyNumbersController.new
+	end
+  
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_template :index
+    assert_not_nil assigns(:pretty_numbers)
+  end
+
+  test "should get pretty" do
+  	post :prettify, number: {value: '1234'}, :format => 'js'
+  	assert_response :success
+  end
 end
