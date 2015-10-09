c6018eea6312fc5ef2c7e4963d32064063628b3c
Daniel Martinez <roanhawkeye@gmail.com>
2015-10-08 20:33:19 -0500

    Add prettify tests

--- test/models/pretty_number_test.rb
+++ test/models/pretty_number_test.rb
@@ -117,4 +117,50 @@ class PrettyNumberTest < ActiveSupport::TestCase
   	pretty_number = PrettyNumber.new(pretty_number_params)
   	assert_not pretty_number.save
   end
+
+  test "get_available_scale return all pretty_number in database" do
+  	#Count actual pretty_number register in database
+  	expected_scales_retrieve = PrettyNumber.all.count
+
+  	all_scales = PrettyNumber.get_available_scales().count
+  
+  	assert_equal(expected_scales_retrieve, all_scales) 
+  end
+
+  test "clean_number should truncate decimal part of number and return string" do
+  	
+  	sample_number = 1234.23
+  	expected_number = '1234'
+  	clean_number = PrettyNumber.clean_number(sample_number)
+  	assert_equal(expected_number, clean_number)
+  
+  end
+
+  test "prettify should return no decimal and appropiate scale" do
+  	sample_number = '1000000'
+  	expected_pretty_version = '1M'
+  	pretty_version = PrettyNumber.prettify(sample_number)
+  	assert_equal(expected_pretty_version, pretty_version)
+  end
+
+  test "prettify should return decimal and appropiate scale" do
+  	sample_number = '2500000.34'
+  	expected_pretty_version = '2.5M'
+  	pretty_version = PrettyNumber.prettify(sample_number)
+  	assert_equal(expected_pretty_version, pretty_version)
+  end
+
+  test "prettify should return same integer number if less than 6 digits length" do
+  	sample_number = '532'
+  	expected_pretty_version = '532'
+  	pretty_version = PrettyNumber.prettify(sample_number)
+  	assert_equal(expected_pretty_version, pretty_version)
+  end
+
+  test "prettify should return truncate number just one decimal with apropiate scale if lot of diferent number" do
+  	sample_number = '1123456789'
+  	expected_pretty_version = '1.1B'
+  	pretty_version = PrettyNumber.prettify(sample_number)
+  	assert_equal(expected_pretty_version, pretty_version)
+  end
 end
